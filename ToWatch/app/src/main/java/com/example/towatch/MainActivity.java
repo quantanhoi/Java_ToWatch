450a63345b7bef521380e69b012ec53373477509
quantanhoi <thieuquangtrung1999@gmail.com>
2023-03-10 19:38:48 +0100

    update ui

--- ToWatch/app/src/main/java/com/example/towatch/MainActivity.java
+++ ToWatch/app/src/main/java/com/example/towatch/MainActivity.java
@@ -1,69 +1,93 @@
 package com.example.towatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.MenuItem;
 
 import com.example.towatch.adapter.MovieAdapter;
 import com.example.towatch.model.Movie;
 import com.example.towatch.model.Result;
 import com.example.towatch.service.RetrofitInstance;
 import com.example.towatch.service.getMovieDataService;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
 
-public class MainActivity extends AppCompatActivity {
-    ArrayList<Movie> movies;
-    MovieAdapter adapter;
-    RecyclerView recyclerView;
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+//    ArrayList<Movie> movies;
+//    MovieAdapter adapter;
+//    RecyclerView recyclerView;
+    BottomNavigationView bottomNavigationView;
+    MyListFragment myListFragment = new MyListFragment();
+    PopularFragment popularFragment = new PopularFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        recyclerView = findViewById(R.id.recyclerView_movie1);
 //        GetMovies();
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.PopularFilm);
     }
 
-    private Object GetMovies() {
-        getMovieDataService getMovieDataService = RetrofitInstance.getService();
-        Call<Result> call = getMovieDataService.getResult();
-        call.enqueue(new Callback<Result>() {
-            @Override
-            public void onResponse(Call<Result> call, Response<Result> response) {
-                Result result = response.body();
-                if(result != null && result.getResults() != null) {
-                    movies = (ArrayList<Movie>) result.getResults();
-                    for(Movie m : movies) {
-                        System.out.println(m.getTitle());
-                    }
-                    ViewData();
-                }
-            }
+//    private Object GetMovies() {
+//        getMovieDataService getMovieDataService = RetrofitInstance.getService();
+//        Call<Result> call = getMovieDataService.getResult();
+//        call.enqueue(new Callback<Result>() {
+//            @Override
+//            public void onResponse(Call<Result> call, Response<Result> response) {
+//                Result result = response.body();
+//                if(result != null && result.getResults() != null) {
+//                    movies = (ArrayList<Movie>) result.getResults();
+//                    for(Movie m : movies) {
+//                        System.out.println(m.getTitle());
+//                    }
+//                    ViewData();
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Result> call, Throwable t) {
+//
+//            }
+//        });
+//        return movies;
+//    }
+//
+//    private void ViewData() {
+//        adapter = new MovieAdapter(movies);
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+//        recyclerView.setLayoutManager(layoutManager);
+//        recyclerView.setAdapter(adapter);
+//
+//    }
 
-            @Override
-            public void onFailure(Call<Result> call, Throwable t) {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, profileFragment).commit();
+                return true;
+            case R.id.PopularFilm:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, popularFragment).commit();
+                return true;
+            case R.id.MyList:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, myListFragment).commit();
+                return true;
 
-            }
-        });
-        return movies;
-    }
-
-    private void ViewData() {
-        adapter = new MovieAdapter(movies);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
-
+        }
+        return false;
     }
 }
--- ToWatch/app/src/main/java/com/example/towatch/PopularFragment.java
+++ ToWatch/app/src/main/java/com/example/towatch/PopularFragment.java
@@ -3,10 +3,24 @@ package com.example.towatch;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.towatch.adapter.MovieAdapter;
+import com.example.towatch.model.Movie;
+import com.example.towatch.model.Result;
+import com.example.towatch.service.RetrofitInstance;
+import com.example.towatch.service.getMovieDataService;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +29,9 @@ import android.view.ViewGroup;
  */
 public class PopularFragment extends Fragment {
 
+    ArrayList<Movie> movies;
+    MovieAdapter adapter;
+    RecyclerView recyclerView;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -52,6 +69,7 @@ public class PopularFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -59,6 +77,45 @@ public class PopularFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_popular, container, false);
+        View view = inflater.inflate(R.layout.fragment_popular, container, false);
+
+        //set recycle view ID to recycle view id in this view (popular fragment) which inflated above
+        //therefore recycleView = view.findViewId
+        //if we assign id of recycleView before inflating, recycleView can't not find the id inside popularview
+        //therefore recycleView is still null => error
+        recyclerView = view.findViewById(R.id.recyclerView_movie1);
+        GetMovies();
+        return view;
+    }
+    private Object GetMovies() {
+        getMovieDataService getMovieDataService = RetrofitInstance.getService();
+        Call<Result> call = getMovieDataService.getResult();
+        call.enqueue(new Callback<Result>() {
+            @Override
+            public void onResponse(Call<Result> call, Response<Result> response) {
+                Result result = response.body();
+                if(result != null && result.getResults() != null) {
+                    movies = (ArrayList<Movie>) result.getResults();
+                    for(Movie m : movies) {
+                        System.out.println(m.getTitle());
+                    }
+                    ViewData();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Result> call, Throwable t) {
+
+            }
+        });
+        return movies;
+    }
+
+    private void ViewData() {
+        adapter = new MovieAdapter(movies);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
     }
 }
